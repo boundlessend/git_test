c31898636a2e59cd014f69c1660def192d3fbace
boundlessend <ohrimmmmm@yahoo.com>
2026-03-27 00:13:37 +0300

    Revert "expose internal is_deleted field in users response"
    This reverts commit 5476b42ab48c0e641edde77bf586354481aed8f9.

--- src/users/repository.py
+++ src/users/repository.py
@@ -1,9 +1,9 @@
 USERS = [
-    {"id": 1, "email": "ivan@example.com", "status": "active", "is_deleted": False},
-    {"id": 2, "email": "anna@example.com", "status": "blocked", "is_deleted": False},
-    {"id": 3, "email": "petr@example.com", "status": "pending", "is_deleted": False},
-    {"id": 4, "email": "olga@example.com", "status": "active", "is_deleted": False},
-    {"id": 5, "email": "max@example.com", "status": "blocked", "is_deleted": True},
+    {"id": 1, "email": "ivan@example.com", "status": "active"},
+    {"id": 2, "email": "anna@example.com", "status": "blocked"},
+    {"id": 3, "email": "petr@example.com", "status": "pending"},
+    {"id": 4, "email": "olga@example.com", "status": "active"},
+    {"id": 5, "email": "max@example.com", "status": "blocked"},
 ]
 
 
